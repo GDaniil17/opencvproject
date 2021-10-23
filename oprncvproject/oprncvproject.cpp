2c666b8b142b71d645fa790d31e616590b5f1fd2
Daniil <galimovdv@yandex.ru>
2021-10-23 15:50:55 +0300

    Added picture crop

--- oprncvproject/oprncvproject.cpp
+++ oprncvproject/oprncvproject.cpp
@@ -7,13 +7,19 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	string path = "C:/Users/Daniil/Desktop/TRASH/1080p.mp4";
-	VideoCapture cap(path);
-	Mat img;
+	string path = "C:/Users/Daniil/Desktop/oprncvproject/oprncvproject/5217954236.jpg";
+	Mat img = imread(path);
+	Mat imgWarp;
+	Point2f src[4] = { {463, 160}, { 818, 307 }, { 241, 656 }, { 600, 813 } };
+	Point2f dst[4] = { {0.0f, 0.0f}, { 300, 0.0f }, { 0.0f, 400 }, { 300, 400 } };
 
-	while (true) {
-		cap.read(img);
-		imshow("Image", img);
-		waitKey(1);
+	Mat matrix = getPerspectiveTransform(src, dst);
+	warpPerspective(img, imgWarp, matrix, Point(300, 400));
+
+	for (auto i : src) {
+		circle(img, i, 10, Scalar(0, 0, 255), FILLED);
 	}
+	imshow("Image", img);
+	imshow("Image Warp", imgWarp);
+	waitKey(0);
 }
